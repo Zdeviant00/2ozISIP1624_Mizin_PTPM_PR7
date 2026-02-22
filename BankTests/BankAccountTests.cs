c15c22bafd58323fa364f26fa700ebff18053dfb
Zdevi <Zdevi@PARVUS_M>
2026-02-23 02:57:57 +0300

    Добавлены результаты автоматизированного тестирования

---
+++ BankTests/BankAccountTests.cs
@@ -0,0 +1,26 @@
+﻿using BankAccountNS;
+
+namespace BankTests
+{
+    [TestClass]
+    public sealed class BankAccountTests
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            decimal beginningBalance = 11.99m;
+            decimal debitAmount = 4.55m;
+            decimal expected = 7.44m;
+            BankAccount account = new BankAccount("Mr. Pelevin V.", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            decimal actual = account.Balance;
+            Assert.AreEqual(expected, actual, "Account not debited correctly");
+        }
+
+    }
+}
