eaa770a7161d65988f72046276c09d9525191418
Zdevi <Zdevi@PARVUS_M>
2026-02-23 13:39:01 +0300

    Исправлен баг в методе Debit

--- Bank/BankAccount.cs
+++ Bank/BankAccount.cs
@@ -41,7 +41,7 @@ namespace BankAccountNS
                 throw new ArgumentOutOfRangeException("amount");
             }
 
-            m_balance += amount;
+            m_balance -= amount;
         }
 
         public void Credit(decimal amount)
